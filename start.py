b1ce06440ad15d71dd5eb958a0c98e7ff4d39eea
Jan Babák <babakhonza@gmail.com>
2023-08-04 09:37:28 +0200

    Add: logging, make Encrypt singleton

--- start.py
+++ start.py
@@ -1,5 +1,12 @@
-from service.encryption import encrypt_file
+from service.encryption import Encrypt
+import logging.config
+
+logging.config.fileConfig("logger.conf")
 
 secret = "secret key"
 
-encrypt_file("file", secret)
+# Encrypt.getInstance.encrypt_file("file", secret)
+
+encryptService = Encrypt.get_instance()
+
+encryptService.encrypt_file("file", secret)
